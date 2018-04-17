3cd1fce9d10c6b2da4e230f2f852d6b7902404b9
atomicguy <adam@atompowered.net>
2018-04-17 16:36:53 +0200

    add detph maps to occlusion processing

--- simulants/composite_with_occlusion.py
+++ simulants/composite_with_occlusion.py
@@ -5,6 +5,7 @@ from __future__ import absolute_import
 import os
 import sys
 import random
+import OpenEXR
 import numpy as np
 
 from PIL import Image, ImageChops
@@ -15,28 +16,14 @@ from combine_layers import generate_overlay
 from combine_layers import matching_method
 from combine_layers import generate_mask
 
+from dataset_toolbox.src.tools import cli, common
+
 from pycocotools.coco import COCO
-
-
-def mkdirp(path):
-    """Checks if a directory exists and if not creates the directory"""
-    if not os.path.exists(path):
-        os.makedirs(path)
-
-
-def list_files(path_to_annotations, file_extension):
-    """Get list of files in a given directory"""
-    file_list = []
-    for file in os.listdir(path_to_annotations):
-        if file.endswith('.' + file_extension):
-            file_path = os.path.join(path_to_annotations, file)
-            file_list.append(file_path)
-    return file_list
 
 
 def patterns_path(pattern_dir):
     """Return a random texture path"""
-    patterns = list_files(pattern_dir, 'png')
+    patterns = common.read_in_files(pattern_dir, 'png', return_paths=True)
     patterns = sorted(patterns)
 
     return random.choice(patterns)
@@ -44,39 +31,15 @@ def patterns_path(pattern_dir):
 
 def background_path(background_dir):
     """Return random background image"""
-    backgrounds = list_files(background_dir, 'jpg')
+    backgrounds = common.read_in_files(background_dir, 'jpg', return_paths=True)
     backgrounds = sorted(backgrounds)
 
     return random.choice(backgrounds)
 
 
-def progress_bar(progress, bar_length=30):
-    """Displays or updates a console progress bar
-
-    :param progress: a float percentage between 0 and 1 (i.e. halt to 100%).
-    :param bar_length: characters wide the progress bar should be
-    """
-    status = ""
-    if isinstance(progress, int):
-        progress = float(progress)
-    if not isinstance(progress, float):
-        progress = 0
-        status = "error: progress var must be float\r\n"
-    if progress < 0:
-        progress = 0
-        status = "Halt...\r\n"
-    if progress >= 1:
-        progress = 1
-        status = "Done...\r\n"
-    block = int(round(bar_length*progress))
-    text = "\rPercent: [{0}] {1}% {2}".format("="*block + " "*(bar_length-block), round(progress*100,4), status)
-    sys.stdout.write(text)
-    sys.stdout.flush()
-
-
 def simulant_id(simulant_dir):
     """Return random simulant id"""
-    simulant_paths = list_files(os.path.join(simulant_dir, 'image_combined'), 'png')
+    simulant_paths = common.read_in_files(os.path.join(simulant_dir, 'image_combined'), 'png', return_paths=True)
     simulant_paths = sorted(simulant_paths)
     simulant_path = random.choice(simulant_paths)
 
@@ -86,14 +49,18 @@ def simulant_id(simulant_dir):
 def layer_paths(simulant_path, simulant_id):
     base_name = '{}.png'.format(simulant_id)
     person = os.path.join(simulant_path, 'image_combined', base_name)
-    skin = os.path.join(simulant_path, 'body_material_index', base_name)
+    body = os.path.join(simulant_path, 'body_material_index', base_name)
     head = os.path.join(simulant_path, 'head_material_index', base_name)
     shirt = os.path.join(simulant_path, 'shirt_material_index', base_name)
     pants = os.path.join(simulant_path, 'pants_material_index', base_name)
     hair = os.path.join(simulant_path, 'hair_material_index', base_name)
+    etc = os.path.join(simulant_path, 'etc_material_index', base_name)
     ao = os.path.join(simulant_path, 'ambient_occlusion', base_name)
+    uv = os.path.join(simulant_path, 'uv', simulant_id + '.exr')
+    depth = os.path.join(simulant_path, 'z', simulant_id + '.exr')
 
-    return {'person': person, 'skin': skin, 'head': head, 'shirt': shirt, 'pants': pants, 'hair': hair, 'ao': ao}
+    return {'person': person, 'body': body, 'head': head, 'shirt': shirt, 'pants': pants, 'hair': hair, 'ao': ao,
+            'uv': uv, 'depth': depth, 'etc': etc}
 
 
 def generate_person_overlay(simulant_id, background_path, simulant_dir, pattern_dir):
@@ -103,16 +70,17 @@ def generate_person_overlay(simulant_id, background_path, simulant_dir, pattern_
     shirt_texture = patterns_path(pattern_dir)
     pants_texture = patterns_path(pattern_dir)
 
-    assert len(layers) == 7, 'layers contains {} elements'.format(len(layers))
+    assert len(layers) == 10, 'layers contains {} elements'.format(len(layers))
 
-    person, clothes, head, body = make_clothed_person(layers['person'], layers['skin'], layers['shirt'], layers['pants'],
+    person, clothes, head, body = make_clothed_person(layers['person'], layers['body'], layers['shirt'], layers['pants'],
                                                 layers['hair'], layers['ao'], layers['head'], pants_texture,
-                                                shirt_texture)
+                                                shirt_texture, layers['uv'], layers['etc'])
 
-    person, clothes_mask, head_mask, body_mask = generate_overlay(person, clothes, head, body, background_path, '',
-                                                           scale_min=0.1, scale_max=1.1)
+    person, clothes_mask, head_mask, body_mask, depth = generate_overlay(person, clothes, head, body, background_path,
+                                                                         '', 'BILINEAR', layers['depth'], scale_min=0.1,
+                                                                         scale_max=1.1)
 
-    return {'person': person, 'clothes': clothes_mask, 'head': head_mask, 'body': body_mask}
+    return {'person': person, 'clothes': clothes_mask, 'head': head_mask, 'body': body_mask, 'depth': depth}
 
 
 def ensure_dirs(path, name_list):
@@ -121,7 +89,7 @@ def ensure_dirs(path, name_list):
     paths = {}
     for name in name_list:
         dir_path = os.path.join(path, name)
-        mkdirp(dir_path)
+        common.mkdirp(dir_path)
         paths[name] = dir_path
 
     return paths
@@ -143,8 +111,6 @@ def np_array_to_mask(numpy_array):
     """Convert numpy array of ones and zeros into PIL 255 RGBA image"""
     if np.max(numpy_array) > 1:
         numpy_array = np.minimum(np.ones_like(numpy_array), numpy_array)
-    # assert np.max(numpy_array) <= 1, 'max value is {}'.format(np.max(numpy_array))
-    # assert np.min(numpy_array) == 0, 'min value is {}'.format(np.min(numpy_array))
     mask_uint8 = (numpy_array * 255).astype(np.uint8)
     ones = np.ones_like(mask_uint8) * 255
     mask_rgba = np.stack([ones, ones, ones, mask_uint8], axis=2)
@@ -195,6 +161,18 @@ def save_layer_mask(layers, layer_name, mask, comp_id, out_paths):
     occluded_mask.save(os.path.join(out_paths[layer_name], comp_id))
 
 
+def save_masked_depth(depth, mask, comp_id, out_paths):
+    comp_id = os.path.splitext(comp_id)[0]
+    depth_array = np.asarray(depth).astype(np.float32)
+    mask_array = np.asarray(mask).copy()
+    mask_array[mask_array > 0] = 1
+    mask_array = np.ones_like(mask_array) - mask_array
+    depth_data = depth_array * mask_array.astype(np.float32)
+    exr = OpenEXR.OutputFile(os.path.join(out_paths['depth'], comp_id + '.exr'), OpenEXR.Header(depth.size[0],
+                                                                                                depth.size[1]))
+    exr.writePixels({'R': depth_data, 'G': depth_data, 'B': depth_data})
+
+
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--simulant_dir', type=str, help='directory containing rendered simulant layers')
@@ -212,10 +190,10 @@ if __name__ == '__main__':
     image_ids_with_people = coco.getImgIds(catIds = [1])
     image_ids_peoplefree = list(set(image_ids) - set(image_ids_with_people))
 
-    out_paths = ensure_dirs(args.out, ['image', 'person', 'head', 'clothes', 'body', 'occlusion'])
+    out_paths = ensure_dirs(args.out, ['image', 'person', 'head', 'clothes', 'body', 'occlusion', 'depth'])
 
     for i in range(args.number):
-        progress_bar((i+1)/args.number)
+        cli.progress_bar((i+1)/args.number)
         thing_mask, image_id = all_annotations_mask(image_ids_peoplefree, randomize=True)
         base_image, base_path = load_image(image_id, args.coco_images)
         sim_id = simulant_id(args.simulant_dir)
@@ -235,6 +213,7 @@ if __name__ == '__main__':
         save_layer_mask(middle_layers, 'clothes', top_mask, comp_id, out_paths)
         save_layer_mask(middle_layers, 'head', top_mask, comp_id, out_paths)
         save_layer_mask(middle_layers, 'body', top_mask, comp_id, out_paths)
+        save_masked_depth(middle_layers['depth'], top_mask, comp_id, out_paths)
 
         top_two_layers = Image.alpha_composite(person, top_layer)
         full_composite = Image.alpha_composite(base_image, top_two_layers)
