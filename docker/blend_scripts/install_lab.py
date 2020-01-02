a1d8999fa079887ad3cd684fb7c5590711b50575
Adam Schuster <adam@atompowered.net>
2020-01-02 15:38:13 -0500

    update to blener 2.81 and MBLab 1.7.7

--- docker/blend_scripts/install_lab.py
+++ docker/blend_scripts/install_lab.py
@@ -2,8 +2,8 @@ import os
 import bpy
 
 bpy.ops.preferences.addon_install(filepath='/tmp/manuelbastionilab.zip')
-os.rename('/root/.config/blender/2.80/scripts/addons/MB-Lab-1.7.4b/',
-          '/root/.config/blender/2.80/scripts/addons/MB-Lab-174b')
-bpy.ops.preferences.addon_enable(module="MB-Lab-174b")
+# os.rename('/root/.config/blender/2.80/scripts/addons/MB-Lab-1.7.4b/',
+#           '/root/.config/blender/2.80/scripts/addons/MB-Lab-174b')
+bpy.ops.preferences.addon_enable(module="MB-Lab-1_7_7")
 bpy.ops.wm.open_mainfile(filepath='/tmp/blend_scripts/base_scene.blend')
 bpy.ops.wm.save_userpref()
